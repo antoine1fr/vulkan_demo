b465bc15431b3ae5ae408a3728108f1555ad2d26
Antoine Luciani <antoine1fr@gmail.com>
2021-10-05 13:50:20 +0200

    Fix coding style inconsistencies

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,10 +1,10 @@
 #version 450
 
-layout (location = 0) in vec3 fragColor;
-layout(location = 1) in vec2 uv;
+layout (location = 0) in vec3 frag_color;
+layout (location = 1) in vec2 uv;
 
-layout(location = 0) out vec4 outColor;
+layout(location = 0) out vec4 out_color;
 
 void main() {
-    outColor = vec4(fragColor, 1.0);
+    out_color = vec4(frag_color, 1.0);
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -4,7 +4,7 @@ layout(location = 0) in vec2 position;
 layout(location = 1) in vec3 color;
 layout(location = 2) in vec2 uv;
 
-layout(location = 0) out vec3 fragColor;
+layout(location = 0) out vec3 frag_color;
 layout(location = 1) out vec2 uv_out;
 
 layout (set = 0, binding = 0) uniform PassUniforms {
@@ -21,6 +21,6 @@ void main() {
     * pass_uniforms.view_matrix
     * object_uniforms.world_matrix
     * vec4(position, 0.0, 1.0);
-  fragColor = color;
+  frag_color = color;
   uv_out = uv;
 }
