b49f7cca249187383eb1b4664b7fdb98d49bdac7
Antoine Luciani <antoine1fr@gmail.com>
2021-10-06 22:31:16 +0200

    Free up descriptor pools

--- include/render/vulkan/DescriptorPoolCache.hpp
+++ include/render/vulkan/DescriptorPoolCache.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vulkan/vulkan.h>
+#include <list>
 #include <vector>
 
 namespace render {
@@ -8,11 +9,13 @@ namespace vulkan {
 class DescriptorPoolCache {
  public:
   DescriptorPoolCache(VkDevice device);
+  ~DescriptorPoolCache();
   VkDescriptorPool GetPool(size_t descriptor_count,
                            const std::vector<VkDescriptorPoolSize>& sizes);
 
  private:
   VkDevice device_;
+  std::list<VkDescriptorPool> pools_;
 };
 }  // namespace vulkan
 }  // namespace render
